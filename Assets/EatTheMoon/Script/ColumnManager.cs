9098c5287e76a12a2f2025b8342bc1c72cdef81d
h-nishihata <nishihata0301@gmail.com>
2019-01-21 20:52:16 +0900

    initially added

---
+++ Assets/EatTheMoon/Script/ColumnManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColumnManager : MonoBehaviour
+{
+    public Slider mainSlider;
+    public GameObject[] elements;
+
+    // Update is called once per frame
+    public void ChangeNumElements()
+    {
+        var num = (int)mainSlider.value;
+        //Debug.Log("value: " + num);
+        for (int i = 0; i < elements.Length; i++)
+        {
+            if (i < num) elements[i].SetActive(true); else elements[i].SetActive(false);
+        }
+    }
+}
