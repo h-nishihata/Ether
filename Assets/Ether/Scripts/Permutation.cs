714ccb9bae1cb48e2f905ea86f3c0838290daab6
h-nishihata <nishihata0301@gmail.com>
2019-02-01 16:30:28 +0900

    reconstruct

---
+++ Assets/Ether/Scripts/Permutation.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+
+public class Permutation : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+        List<string> result = new List<string>();
+        List<string> ABCD = new List<string>() { "2", "3", "4", "5" };
+        permutation(ABCD, result);
+
+
+    }
+
+    void permutation(List<string> ABCD, List<string> result)
+    {
+        int count = ABCD.Count;
+
+        for (int i = 0; i < count; i++)
+        {
+            result.Add(ABCD[i]);
+            ABCD.RemoveAt(i);
+
+            if (count >= 2)
+            {
+                permutation(ABCD, result);
+                ABCD.Insert(i, result[result.Count - 1]);
+                result.RemoveAt(result.Count - 1);
+            }
+
+            if (count == 1)
+            {
+                string[] tmp = result.ToArray();
+                Debug.Log(string.Join(",", tmp));
+                ABCD.Insert(i, result[result.Count - 1]);
+                result.RemoveAt(result.Count - 1);
+            }
+
+
+        }
+
+    }
+}
---
+++ Assets/Ether/Scripts/TurnPage.cs
@@ -0,0 +1,57 @@
+﻿using DG.Tweening;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(SwipeGesture), typeof(HorizontalLayoutGroup))]
+public class TurnPage : MonoBehaviour
+{
+    public float PageWidth = 1080;
+
+    private RectTransform rectTransform;
+    private SwipeGesture swipeGesture;
+    private Tween moveAnimation;
+    private int pageCount;
+    private int currentPage = 1;
+
+    void Awake()
+    {
+        DOTween.Init();
+        DOTween.defaultAutoPlay = AutoPlay.None; // Tween生成時に自動再生させない
+    }
+
+    void OnEnable()
+    {
+        this.rectTransform = this.GetComponent<RectTransform>();
+        this.swipeGesture = this.GetComponent<SwipeGesture>();
+        this.pageCount = this.transform.childCount;
+
+        // next
+        this.swipeGesture
+            .OnSwipeLeft
+            .Where(_ => currentPage < pageCount) // 最大ページ以前である場合のみ進める
+            .Where(_ => this.moveAnimation == null || !this.moveAnimation.IsPlaying()) // アニメーション実行中ではない
+            .Subscribe(_ =>
+            {
+                this.currentPage++;
+                this.moveAnimation = this.rectTransform
+                    .DOAnchorPosX(rectTransform.anchoredPosition.x - this.PageWidth, 1.0f)
+                    .SetEase(Ease.OutBounce)
+                    .Play();
+            });
+
+        // back
+        this.swipeGesture
+            .OnSwipeRight
+            .Where(_ => currentPage > 1) // 1ページ目以降である場合のみ戻れる
+            .Where(_ => this.moveAnimation == null || !this.moveAnimation.IsPlaying()) // アニメーション実行中ではない
+            .Subscribe(_ =>
+            {
+                this.currentPage--;
+                this.moveAnimation = this.rectTransform
+                    .DOAnchorPosX(rectTransform.anchoredPosition.x + this.PageWidth, 1.0f)
+                    .SetEase(Ease.OutBounce)
+                    .Play();
+            });
+    }
+}
