18794cd79dda2fce9fda1da546521fc8d3f65a08
h-nishihata <nishihata0301@gmail.com>
2019-02-06 19:30:46 +0900

    fix resolution

--- Assets/Ether/Scripts/TitleSceneControl.cs
+++ Assets/Ether/Scripts/TitleSceneControl.cs
@@ -3,23 +3,32 @@ using UnityEngine.SceneManagement;
 
 public class TitleSceneControl : MonoBehaviour {
    
-	public Texture	TitleTexture = null;
-	public SimpleSpriteGUI	title;
-    private GUIStyle guiStyle;
+	private GUIStyle guiStyle;
     private GUIStyleState styleState;
+
     public string version = "Version_0.2.0";
+    public int gameWindowWidth = 414;
+    public int gameWindowHeight = 896;
 
-	void Start ()
+
+    void Awake()
     {
-		this.title = new SimpleSpriteGUI();
-		this.title.create();
-		this.title.setTexture(this.TitleTexture);
+        if (Application.platform == RuntimePlatform.WindowsPlayer ||
+            Application.platform == RuntimePlatform.OSXPlayer ||
+            Application.platform == RuntimePlatform.LinuxPlayer)
+        {
+            Screen.SetResolution(gameWindowWidth, gameWindowHeight, false);
+        }
+    }
 
+    void Start ()
+    {
         guiStyle = new GUIStyle();
-        guiStyle.fontSize = 48;
         styleState = new GUIStyleState();
-        styleState.textColor = Color.black;
+
+        guiStyle.fontSize = 48;
         guiStyle.normal = styleState;
+        styleState.textColor = Color.black;
 	}
 
     void Update()
@@ -32,7 +41,6 @@ public class TitleSceneControl : MonoBehaviour {
 
 	void OnGUI()
 	{
-		this.title.draw();
         GUI.Label(new Rect(800, 2500, 200, 200), version, guiStyle);
 	}
 }
