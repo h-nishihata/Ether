fcc8a9fb794ff4288bcb97f3ace782e68e2c6d56
h-nishihata <nishihata0301@gmail.com>
2019-02-09 18:39:37 +0900

    generate lot number

--- Assets/SetParticleImages.cs
+++ Assets/SetParticleImages.cs
@@ -1,21 +1,24 @@
 ﻿using System;
+using System.Text;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class SetParticleImages : MonoBehaviour
 {
     public GameObject[] particles;
+    public GameObject number;
     private CSVReader csvReader;
     public int pageID;
     private int numBoxes;
     private int initLine;
-
+    private StringBuilder lotNumber = new StringBuilder();
 
     private void OnEnable()
     {
         numBoxes = Data.Instance.numBoxes;
         initLine = Data.Instance.csvInitLine;
         csvReader = transform.parent.GetComponent<CSVReader>();
+        lotNumber.Append("1");
         PrepareBoxes();
         SetImages();
     }
@@ -37,8 +40,19 @@ public class SetParticleImages : MonoBehaviour
         for (int i = 0; i < numBoxes; i++)
         {
             var images = particles[i].GetComponent<Image>();
-            var numImages = Int32.Parse(csvReader.csvData[initLine + pageID][i]);
-            images.sprite = csvReader.sourceImages[numImages - 2]; //2〜7段目が可変パーティクルになるよう設定.
+            var numImages = csvReader.csvData[initLine + pageID][i];
+            lotNumber.Append(numImages);
+            var imageID = Int32.Parse(numImages);
+            images.sprite = csvReader.sourceImages[imageID - 2]; //2〜7段目が可変パーティクルになるよう設定.
         }
+
+        SetLotNumber();
+    }
+
+    private void SetLotNumber()
+    {
+        lotNumber.Append("8");
+        //Debug.Log(lotNumber);
+        number.GetComponent<Text>().text = lotNumber.ToString();
     }
 }
