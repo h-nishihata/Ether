2ed899ed6844c62589c4636642f41171e04fc52f
h-nishihata <nishihata0301@gmail.com>
2019-07-07 16:42:36 +0900

    fix

--- Assets/Ether/Scripts/SetMatTexure.cs
+++ Assets/Ether/Scripts/SetMatTexure.cs
@@ -4,25 +4,30 @@ public class SetMatTexure : MonoBehaviour {
 
     public Material textureMat;
     public Texture[] textures;
+    public Texture[] normalMaps;
 
 
     // Use this for initialization
     void Start () {
         textureMat.SetTexture("_MainTex", textures[0]);
+        textureMat.SetTexture("_BumpMap", normalMaps[0]);
     }
 
     public void SetDefaultTexture()
     {
         textureMat.SetTexture("_MainTex", textures[0]);
+        textureMat.SetTexture("_BumpMap", normalMaps[0]);
     }
 
     public void SetTextureA()
     {
         textureMat.SetTexture("_MainTex", textures[1]);
+        textureMat.SetTexture("_BumpMap", null);
     }
 
     public void SetTextureB()
     {
         textureMat.SetTexture("_MainTex", textures[2]);
+        textureMat.SetTexture("_BumpMap", null);
     }
 }
