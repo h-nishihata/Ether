8fa8bf87c335268c94b961fb5f30247ca61270f7
h-nishihata <nishihata0301@gmail.com>
2019-08-11 16:27:29 +0900

    fix

---
+++ Assets/SelectorBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectorBehaviour : MonoBehaviour
+{
+    public RectTransform[] targets;
+
+    public void MoveTo(int targetNum)
+    {
+        transform.position = targets[targetNum].position;
+        //gameObject.transform.Translate(targets[targetNum].position * Time.deltaTime);
+        //transform.position = Vector2.MoveTowards(transform.position, targets[targetNum].position, Time.deltaTime);
+    }
+}
