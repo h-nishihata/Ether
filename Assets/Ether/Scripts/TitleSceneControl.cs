6b19d1919bc93ab222732cd9f65f6675483abb6b
h-nishihata <nishihata0301@gmail.com>
2019-08-13 18:31:21 +0900

    update title scene

--- Assets/Ether/Scripts/TitleSceneControl.cs
+++ Assets/Ether/Scripts/TitleSceneControl.cs
@@ -2,45 +2,47 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class TitleSceneControl : MonoBehaviour {
-   
-	private GUIStyle guiStyle;
-    private GUIStyleState styleState;
 
-    public string version = "Version_0.2.0";
-    public int gameWindowWidth = 414;
-    public int gameWindowHeight = 896;
-
+    private int screenWidth;
+    private int screenHeight;
+    private GUIStyle guiStyle;
+    public string version = "Ver_1.0.0";
+    //public Transform ether;
+    //private float rotateVal;
 
     void Awake()
     {
+        screenWidth = Screen.width;
+        screenHeight = Screen.height;
+
         if (Application.platform == RuntimePlatform.WindowsPlayer ||
             Application.platform == RuntimePlatform.OSXPlayer ||
             Application.platform == RuntimePlatform.LinuxPlayer)
         {
-            Screen.SetResolution(gameWindowWidth, gameWindowHeight, false);
+            Screen.SetResolution(screenWidth, screenHeight, false);
         }
     }
 
     void Start ()
     {
         guiStyle = new GUIStyle();
-        styleState = new GUIStyleState();
-
-        guiStyle.fontSize = 48;
-        guiStyle.normal = styleState;
-        styleState.textColor = Color.black;
+        guiStyle.fontSize = (int)(screenWidth * 0.02f);
+        guiStyle.normal.textColor = Color.white;
 	}
 
     void Update()
     {
+        //rotateVal += Time.deltaTime * 20f;
+        //ether.transform.rotation = Quaternion.AngleAxis(rotateVal, Vector3.right);
+
         if (Input.GetMouseButtonDown(0))
         {
-            SceneManager.LoadScene("2_Swipe");
+            SceneManager.LoadScene("Main");
         }
     }
 
 	void OnGUI()
 	{
-        GUI.Label(new Rect(800, 2500, 200, 200), version, guiStyle);
+        GUI.Label(new Rect(Screen.width - 200, Screen.height - 100, 200, 100), version, guiStyle);
 	}
 }
