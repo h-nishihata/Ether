26efd025ced16ae827a84156d8741a26b26bbc56
h-nishihata <nishihata0301@gmail.com>
2019-08-19 13:25:40 +0900

    permutation

--- Assets/Ether/Scripts/Permutation.cs
+++ Assets/Ether/Scripts/Permutation.cs
@@ -32,35 +32,51 @@ public class Permutation : MonoBehaviour
     {
         for (int i = 0; i < dropList.Count; i++)
         {
+            //if ((iterCount < 1) && (Random.Range(0f, 10f) > 3f))
+            //continue;
             tempResult.Add(dropList[i]);
 
-            for (int j = 0; j < dropList.Count; j++)
-            {
-                tempResult.Add(dropList[j]);
-
-                if (iterCount < 1)
+            //if (iterCount < 2)
+            //{
+                for (int j = 0; j < dropList.Count; j++)
                 {
-                    iterCount = 1;
-                    GeneratePatterns();
-                }
+                    tempResult.Add(dropList[j]);
 
-                Save(tempResult);
-                tempResult.RemoveAt(tempResult.Count - 1);
-            }
+
+                    //for (int k = 0; k < dropList.Count; k++)
+                    //{
+                        //tempResult.Add(dropList[k]);
+                        if (iterCount < 0)
+                        {
+                            iterCount++;
+                            GeneratePatterns();
+                        }
+                        Save(tempResult);
+                        tempResult.RemoveAt(tempResult.Count - 1);
+                    //}
+                    //Save(tempResult);
+                    //tempResult.RemoveAt(tempResult.Count - 1);
+                }
+            //}
+            //else
+            //{
+            //    Save(tempResult);
+            //}
 
             tempResult.RemoveAt(tempResult.Count - 1);
 
-            if (i == dropList.Count - 1)
-                iterCount = 0;
+            if ((i == dropList.Count - 1) && (iterCount >0))
+                iterCount--;
         }
     }
 
     public void Save(List<string> tempResult)
     {
-        //if (tempResult.Count < 4)
-        //return;
-        if (Random.Range(0f, 10f) > 4.5f) // 膨大な数のパターンが生成されるので，CSVファイルへ書き込む前の段階でランダムに選択する.
+        if (tempResult.Count < 2)
             return;
+
+        //if (Random.Range(0f, 10f) > 1f) // 膨大な数のパターンが生成されるので，CSVファイルへ書き込む前の段階でランダムに選択する.
+            //return;
 
         result.Add("1");
         for (int i = 0; i < tempResult.Count; i++)
@@ -72,16 +88,16 @@ public class Permutation : MonoBehaviour
         }
         result.Add("8");
 
-        if (result.Count == tempResult.Count + 2)
-        {
+        //if (result.Count == tempResult.Count + 2)
+        //{
             var stringData = string.Join(",", result.ToArray());
-            Debug.Log(stringData);
-            //csvWriter.Save(stringData, "patternData");
+            //Debug.Log(stringData);
+            csvWriter.Save(stringData, "patternData");
 
             // ex.
             //stringArray = new string[] { "\n", "1,2,3", "4,5,6", "7,8,9" };
             //csvWriter.Save(stringArray, "patternData");
-        }
+        //}
 
     CLEAR: ;
         result.Clear();
