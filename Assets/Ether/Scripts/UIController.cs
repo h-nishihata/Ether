6185df85bfbdf00a68ff17fed2a5b005f50d2458
h-nishihata <nishihata0301@gmail.com>
2019-11-14 19:49:13 +0900

    add rotation

---
+++ Assets/Ether/Scripts/UIController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    public Transform UIPanel;
+    public bool isActive = true;
+
+    private void Update()
+    {
+        // UIパネル非表示時に，画面をタップすると再度表示する.
+        if (!isActive && Input.GetMouseButtonDown(0))
+            switchUIPanel(true);
+    }
+
+    /// <summary>
+    /// UIパネルの表示/非表示の切替をする.
+    /// </summary>
+    public void switchUIPanel(bool status)
+    {
+        UIPanel.gameObject.SetActive(status);
+        isActive = status;
+    }
+}
