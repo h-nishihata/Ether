088b7e5f972163548cacbc5023c052996e91a7c3
h-nishihata <nishihata0301@gmail.com>
2019-11-15 21:51:19 +0900

    add textures

---
+++ Assets/Ether/Scripts/MatTexSetter.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Scene内の任意の場所(MainCamera)にアタッチされた，マテリアル管理のためのスクリプト.
+/// </summary>
+public class MatTexSetter : MonoBehaviour {
+
+    public Slider slider;
+    public Material textureMat;
+    public Texture[] textures;
+    public Texture[] normalMaps;
+    public Text matTypeInfo;
+
+
+    void Start () {
+        this.SetTexture(0);
+    }
+
+    public void SetTexture(int matType)
+    {
+        if (matType < 0)
+            matType = (int)slider.value;
+    
+
+        textureMat.SetTexture("_MainTex", textures[matType]);
+        textureMat.SetTexture("_BumpMap", normalMaps[matType]);
+        matTypeInfo.text = textures[matType].name;
+    }
+}
